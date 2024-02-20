7e0d80a1cc3c143c364b60c83cf3e020b480398a
Yi Tam <tam.yi3d@gmail.com>
2024-02-20 16:29:28 +0100

    create two schemas for db

---
+++ src/user/schema.ts
@@ -0,0 +1,11 @@
+import {pgTable, serial, text} from "drizzle-orm/pg-core";
+import {relations} from "drizzle-orm";
+import {posts} from "../post/schema.ts";
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+});
+export const usersRelations = relations(users, ({ many }) => ({
+  posts: many(posts),
+}));
